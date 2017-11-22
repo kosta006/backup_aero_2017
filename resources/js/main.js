94584c71488d1a61df83f6599b98586f4196f8c5
Pedro Borges <oi@pedroborg.es>
2017-11-21 22:44:13 -0200

    Install turbolinks

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,19 +1,32 @@
-const menuButton = document.querySelector('.js-menu-btn')
-const menuButtonOpen = document.querySelector('.js-menu-btn--open')
-const menuButtonClose = document.querySelector('.js-menu-btn--close')
-const menuItems = document.querySelector('.js-menu-items')
+window.addEventListener('load', function() {
+    var Turbolinks = require('turbolinks')
 
-// let buttonState = false
+    function startMenu() {
+        const menuButton = document.querySelector('.js-menu-btn')
+        const menuButtonOpen = document.querySelector('.js-menu-btn--open')
+        const menuButtonClose = document.querySelector('.js-menu-btn--close')
+        const menuItems = document.querySelector('.js-menu-items')
 
-menuButton.addEventListener('click', function() {
-    return toggleMenu()
+        menuButton.addEventListener('click', function() {
+            return toggleMenu()
+        })
+
+        function toggleMenu() {
+            menuButton.classList.toggle(menuButton.dataset.text)
+            menuButton.classList.toggle(menuButton.dataset.activeText)
+            menuButton.classList.toggle(menuButton.dataset.activeBackground)
+            menuButtonOpen.classList.toggle('hidden')
+            menuButtonClose.classList.toggle('hidden')
+            menuItems.classList.toggle('hidden')
+        }
+    }
+
+    document.addEventListener('turbolinks:load', function() {
+        startMenu()
+    })
+
+    Turbolinks.start()
+    startMenu()
+
 })
 
-function toggleMenu() {
-    menuButton.classList.toggle('text-blue')
-    menuButton.classList.toggle('text-white')
-    menuButton.classList.toggle('bg-white')
-    menuButtonOpen.classList.toggle('hidden')
-    menuButtonClose.classList.toggle('hidden')
-    menuItems.classList.toggle('hidden')
-}
