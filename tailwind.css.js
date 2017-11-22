bb17943d06bbf54885d8abf4d8366147761eb6d6
Pedro Borges <oi@pedroborg.es>
2017-11-22 15:01:45 -0200

    Add more sizes to tailwind config

--- tailwind.css.js
+++ tailwind.css.js
@@ -129,6 +129,30 @@ var colors = {
   'pink-lightest': '#ffebef',
 }
 
+var sizes = {
+    'px': '1px',
+    '0': '0',
+    '1': '0.25rem',
+    '2': '0.5rem',
+    '3': '0.75rem',
+    '4': '1rem',
+    '5': '1.25rem',
+    '6': '1.5rem',
+    '8': '2rem',
+    '10': '2.5rem',
+    '12': '3rem',
+    '14': '3.5rem',
+    '16': '4rem',
+    '20': '5rem',
+    '24': '6rem',
+    '28': '7rem',
+    '32': '8rem',
+    '40': '10rem',
+    '48': '12rem',
+    '56': '14rem',
+    '64': '16rem',
+}
+
 module.exports = {
 
   /*
@@ -447,22 +471,8 @@ module.exports = {
   |
   */
 
-  width: {
+  width: Object.assign({
     'auto': 'auto',
-    'px': '1px',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '6': '1.5rem',
-    '8': '2rem',
-    '10': '2.5rem',
-    '12': '3rem',
-    '16': '4rem',
-    '24': '6rem',
-    '32': '8rem',
-    '48': '12rem',
-    '64': '16rem',
     '1/2': '50%',
     '1/3': '33.33333%',
     '2/3': '66.66667%',
@@ -476,7 +486,7 @@ module.exports = {
     '5/6': '83.33333%',
     'full': '100%',
     'screen': '100vw'
-  },
+  }, sizes),
 
 
   /*
@@ -494,25 +504,11 @@ module.exports = {
   |
   */
 
-  height: {
+  height: Object.assign({
     'auto': 'auto',
-    'px': '1px',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '6': '1.5rem',
-    '8': '2rem',
-    '10': '2.5rem',
-    '12': '3rem',
-    '16': '4rem',
-    '24': '6rem',
-    '32': '8rem',
-    '48': '12rem',
-    '64': '16rem',
     'full': '100%',
     'screen': '100vh'
-  },
+  }, sizes),
 
 
   /*
@@ -620,17 +616,7 @@ module.exports = {
   |
   */
 
-  padding: {
-    'px': '1px',
-    '0': '0',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '6': '1.5rem',
-    '8': '2rem',
-    '16': '4rem',
-  },
+  padding: sizes,
 
 
   /*
@@ -648,18 +634,9 @@ module.exports = {
   |
   */
 
-  margin: {
+  margin: Object.assign({
     'auto': 'auto',
-    'px': '1px',
-    '0': '0',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '6': '1.5rem',
-    '8': '2rem',
-    '16': '4rem',
-  },
+  }, sizes),
 
 
   /*
@@ -677,17 +654,7 @@ module.exports = {
   |
   */
 
-  negativeMargin: {
-    'px': '1px',
-    '0': '0',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '6': '1.5rem',
-    '8': '2rem',
-    '16': '4rem',
-  },
+  negativeMargin: sizes,
 
 
   /*
