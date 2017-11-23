6ab25af474b0f3e8b29a60b9eea51ab6225d1c1d
Pedro Borges <oi@pedroborg.es>
2017-11-23 10:50:29 -0200

    Improve readability
    - Increase text size on mobile
    - Change text color to dark gray
    - Change headings color to black

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -6,7 +6,7 @@
     <main id="main" class="px-4" role="main">
         <h1 class="mb-6 text-black text-5xl font-hairline">{{ $page->title() }}</h1>
 
-        <div class="content md:mb-24">
+        <div class="content md:mb-24 text-grey-darker text-lg md:text-base">
             {{ $page->text()->kirbytext() }}
         </div>
     </main>
